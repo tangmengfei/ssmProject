9bc7625adf9575818f3837fc9c0945c00294d665
qaz <2289254635@qq.com>
2020-11-06 17:26:52 +0800

    ssmProject代码提交

--- src/main/java/com/tmf/test/controller/UserController.java
+++ src/main/java/com/tmf/test/controller/UserController.java
@@ -2,6 +2,7 @@ package com.tmf.test.controller;
 
 import com.tmf.test.model.User;
 import com.tmf.test.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.logging.Logger;
-
 @Controller
 @ResponseBody
 @RequestMapping("/admin")
+@Api(value = "UserController用户管理", tags = "UserController管理接口Api")
 public class UserController {
 
     @Autowired
---
+++ src/main/java/com/tmf/test/utils/ResultResponse.java
@@ -0,0 +1,10 @@
+package com.tmf.test.utils;
+
+import java.io.Serializable;
+
+public class ResultResponse<T> implements Serializable {
+    private static final long serialVersionUID = -6533147705623177838L;
+
+
+
+}
